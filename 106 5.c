0063de7c9515e180e42328d246c451d32d9d9af3
FreeAaron <jason730lee@gmail.com>
2023-02-13 23:20:35 +0800

    106 5 6

---
+++ 106 5.c
@@ -0,0 +1,144 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include<stdio.h>
+#include<string.h>
+
+int
+test08 ()
+{
+  int i = 0;
+  return i++;
+}
+
+int
+test07 ()
+{
+  int grade = 4, ans = 0;
+  switch (grade)
+    {
+    case 4:
+      ans += 4;
+    case 3:
+      ans += 3;
+    case 2:
+      ans += 2;
+    case 1:
+      ans += 1;
+    default:
+      ans = 0;
+      printf ("%d\n", ans + 7);
+    }
+
+  return ans > 0 ? ans : 0;
+}
+
+
+int
+test06 ()
+{
+  int v = 0xFF;
+  printf ("%d\n", v);
+  v &= 1;
+  printf ("%d\n", v);
+  v |= 0;
+  printf ("%d\n", v);
+  v <<= 3;
+  printf ("%d\n", v);
+  v >>= 1;
+  return v;
+}
+
+
+int
+test05 ()
+{
+  int a[] = { 1, 2, 3, 4, 5 };
+  int *p, *q;
+  p = &a[8];
+  q = p - 3;
+  p -= 6;
+  return p - q;
+}
+
+int
+test04 ()
+{
+  char str1[10], str2[10];
+  strcpy (str1, "abc");
+  strcpy (str2, "abc");
+  strcat (str1, strcat (str2, "ghi"));
+  return strcmp (str1, str2);
+}
+
+int
+test03 (int n)
+{
+  int rem;
+  do
+    {
+      n /= 10;
+      rem = n % 10;
+      if (rem != 0)
+	break;
+    }
+  while (n > 0);
+  return rem;
+}
+
+int
+test02 (void)
+{
+  enum
+  { CLUBS, DIAMONDS, HEARTS, SPADES } s;
+  int i = DIAMONDS;
+  s = HEARTS;
+  s++;  
+  i = i + s + SPADES;
+  return i;
+}
+
+int
+test09 (int n)
+{
+  int i = 1;
+  while (i < n)
+    i *= 2;
+  return i;
+}
+
+int
+test10 (int n)
+{
+  int div;
+  if (n <= 1)
+    return 0;
+  for (div = 2; div * div <= n; div++)
+    if (n % div == 0)
+      return 0;
+  return 1;
+}
+
+
+int
+main (void)
+{
+  // your code goes here
+  printf ("%d\n", test08 ());
+  printf ("%d\n", test07 ());
+  printf ("%d\n", test06 ());
+  printf ("%d\n", test05 ());
+  printf ("%d\n", test04 ());
+  printf ("%d\n", test03 (123));
+  printf ("%d\n", test02 ());
+  printf ("%d\n", test09 (100));
+  printf ("%d\n", test10 (7));
+  return 0;
+}
+
+
---
+++ 106 6.cpp
@@ -0,0 +1,102 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+class Number
+{
+public:
+  Number (int x, int y):real (x)
+  {
+    pImaginary = new int (y);
+  }
+  Number (Number & cn)
+  {
+    int n = (*cn.pImaginary) + 1;
+    real = cn.real + 1;
+    pImaginary = new int (n);
+  }
+  int getNumber ()
+  {
+    return real + (*pImaginary);
+  }
+  void addImaginary (int n)
+  {
+    pImaginary += n;
+  }
+  void newImaginary (int n)
+  {
+    delete pImaginary;
+    pImaginary = new int[n];
+    setImaginary (n);
+  }
+  int getReal ()
+  {
+    return real;
+  }
+  int getImaginary (int i)
+  {
+    return pImaginary[i];
+  }
+  void compute ()
+  {
+    pImaginary = &real;
+  }
+  void compute (Number * cn)
+  {
+    cn = new Number (3, 4);
+  }
+  void compute (Number & cn)
+  {
+    cn = Number (5, 6);
+  }
+  Number compute (Number cn, int n)
+  {
+    cn.real += n;
+    return cn;
+  }
+  void deletemaginary ()
+  {
+    delete pImaginary;
+  }
+
+private:
+  void setImaginary (int n)
+  {
+    for (int i = 0; i < n; i++)
+      pImaginary[i] = i * 2 + 1;
+  }
+  int real;
+  int *pImaginary;
+};
+
+int
+main ()
+{
+  Number cn1 (1, 2);
+  cout << cn1.getNumber () << endl;	//Problem 6-1 
+  Number cn2 (3, 4);
+  cn2.compute ();
+  cout << cn2.getNumber () << endl;	//Problem 6-2
+  Number cn3 (5, 6);
+  cn3.newImaginary (3);
+  cout << cn3.getReal () << endl;	//Problem 6-3
+  Number cn4 (7, 8);
+  cn4.newImaginary (5);
+  cout << cn4.getImaginary (3) << endl;	//Problem 6 - 4 
+  Number cn5 (9, 10);
+  cn5.newImaginary (7);
+  cn5.addImaginary (1);
+  cout << cn5.getNumber () << endl;	//Problem 6-5
+  Number cn6 (11, 12);
+  cout << cn6.compute (cn6,1).getNumber () << endl;	//Problem 6-6
+
+  return 0;
+}
